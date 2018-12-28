9264a9d3549a0fb759b8210541ae4523df691bac
Oliwier Salamon <salamonoliwier@gmail.com>
2018-12-28 19:48:44 +0100

    added navigation for application

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,106 +1,85 @@
-import React, { Component } from "react";
-import { Form, Segment, Button } from "semantic-ui-react";
+import React, { Component } from 'react';
+import { Segment, Form, Button } from 'semantic-ui-react';
 
 const emptyEvent = {
   title: '',
   date: '',
   city: '',
   venue: '',
-  hostedBy: '',
+  hostedBy: ''
 }
 
 class EventForm extends Component {
 
-  state ={ 
-    event: emptyEvent, 
+  state = {
+    event: emptyEvent
   }
 
-  componentDidMount () {
-    if(this.props.selectedEvent !== null){
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
       this.setState({
-        event: this.props.selectedEvent,
+        event: this.props.selectedEvent
       })
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.selectedEvent !==this.props.selectedEvent) {
+    if (nextProps.selectedEvent !== this.props.selectedEvent) {
       this.setState({
-        event: nextProps.selectedEvent || emptyEvent,
+        event: nextProps.selectedEvent || emptyEvent
       })
     }
   }
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    if(this.state.event.id){
-      this.props.updateEvent(this.state.event)
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    if (this.state.event.id) {
+      this.props.updateEvent(this.state.event);
     } else {
-      this.props.createEvent(this.state.event);
+      this.props.createEvent(this.state.event)
     }
-    
+
   }
 
-  onInputChange = (event) => {
-    const newEvent=this.state.event;
-    newEvent[event.target.name]=event.target.value;
+  onInputChange = (evt) => {
+    const newEvent = this.state.event;
+    newEvent[evt.target.name] = evt.target.value
     this.setState({
-      event: newEvent,
+      event: newEvent
     })
   }
 
   render() {
 
-    const {handleCancel}=this.props;
-    const {event}=this.state;
+    const {handleCancel} = this.props;
+    const {event} = this.state;
     return (
       <Segment>
         <Form onSubmit={this.onFormSubmit}>
           <Form.Field>
             <label>Event Title</label>
-            <input 
-            name='title'
-            onChange={this.onInputChange}
-            value={event.title} 
-            placeholder="Event title" />
+            <input name='title' onChange={this.onInputChange} value={event.title} placeholder="Event Title" />
           </Form.Field>
           <Form.Field>
             <label>Event Date</label>
-            <input 
-            name='date'
-            onChange={this.onInputChange}
-            value={event.date}
-            type="date" 
-            placeholder="Event Date" />
+            <input name='date' onChange={this.onInputChange} value={event.date} type="date" placeholder="Event Date" />
           </Form.Field>
           <Form.Field>
             <label>City</label>
-            <input 
-            name='city'
-            onChange={this.onInputChange}
-            value={event.city}
-            placeholder="City event is taking place" />
+            <input name='city' onChange={this.onInputChange} value={event.city} placeholder="City event is taking place" />
           </Form.Field>
           <Form.Field>
             <label>Venue</label>
-            <input 
-            name='venue'
-            onChange={this.onInputChange}
-            value={event.venue}
-            placeholder="Enter the Venue of the event" />
+            <input name='venue' onChange={this.onInputChange} value={event.venue} placeholder="Enter the Venue of the event" />
           </Form.Field>
           <Form.Field>
             <label>Hosted By</label>
-            <input 
-            name='hostedBy'
-            onChange={this.onInputChange}
-            value={event.hostedBy}
-            placeholder="Enter the name of person hosting" />
+            <input name='hostedBy' onChange={this.onInputChange} value={event.hostedBy} placeholder="Enter the name of person hosting" />
           </Form.Field>
           <Button positive type="submit">
             Submit
           </Button>
-          <Button onClick = {handleCancel} type="button">Cancel</Button>
+          <Button onClick={handleCancel} type="button">Cancel</Button>
         </Form>
       </Segment>
     );
--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
-const HomePage = () => {
+const HomePage = ({history}) => {
   return (
     <div>
-      <h1>Witam</h1>
-    </div>
+          <div className="ui inverted vertical masthead center aligned segment">
+            <div className="ui text container">
+              <h1 className="ui inverted stackable header">
+                <img
+                  className="ui image massive"
+                  src="/assets/logo.png"
+                  alt="logo"
+                />
+                <div className="content">Re-vents</div>
+              </h1>
+              <h2>Do whatever you want to do</h2>
+              <div 
+              onClick={()=>{
+                history.push('/events')
+              }}
+              className="ui huge white inverted button">
+                Get Started
+                <i className="right arrow icon" />
+              </div>
+            </div>
+          </div>
+        </div>
   )
 }
 
