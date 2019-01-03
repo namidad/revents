e529e396e42f65cec0bce9e2e48767ab1f275989
Oliwier Salamon <salamonoliwier@gmail.com>
2019-01-03 11:59:24 +0100

    added first steps of redux

--- src/features/nav/Menus/SignedInMenu.jsx
+++ src/features/nav/Menus/SignedInMenu.jsx
@@ -9,6 +9,7 @@ const SignedInMenu = ({signOut}) => {
               <Image avatar spaced="right" src='/assets/user.png' />
               <Dropdown pointing="top left" text="Username">
                 <Dropdown.Menu>
+                    
                   <Dropdown.Item text="Create Event" icon="plus" />
                   <Dropdown.Item text="My Events" icon="calendar" />
                   <Dropdown.Item text="My Network" icon="users" />
