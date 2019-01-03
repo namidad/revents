bd66b8453425e464a6c308e648ae851bfbbeb470
Oliwier Salamon <salamonoliwier@gmail.com>
2019-01-03 13:46:17 +0100

    testing redux

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware} from 'redux'
 import rootReducer from '../reducers/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 export const configureStore = (preloadedState) => {
     const middlewares = [];
@@ -7,13 +8,22 @@ export const configureStore = (preloadedState) => {
 
     const storeEnhancers = [middlewareEnhancer];
 
-    const composedEnhancer = compose(...storeEnhancers);
+    const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
     const store = createStore(
         rootReducer,
         preloadedState,
         composedEnhancer
     );
+
+    if(process.env.NODE_ENV !== 'production'){
+        if(module.hot){
+            module.hot.accept('../reducers/rootReducer', ()=>{
+                const newRootReducer = require('../reducers/rootReducer').default;
+                store.replaceReducer(newRootReducer);
+            });
+        }
+    }
 
     return store;
 }
