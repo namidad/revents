920661bba3ed67788b0298ade5277a873df290b0
Oliwier Salamon <salamonoliwier@gmail.com>
2019-01-04 18:06:29 +0100

    added event detailed page

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import EventDetailedHeader from './EventDetailedHeader'
+import EventDetailedInfo from './EventDetailedInfo'
+import EventDetailedChat from './EventDetailedChat'
+import EventDetailedSidebar from './EventDetailedSidebar'
+import { Grid } from 'semantic-ui-react'
 
 const EventDetailedPage = () => {
   return (
-    <div>
-      <h1>Witam</h1>
-    </div>
+    <Grid>
+      <Grid.Column width={10}>
+        <EventDetailedHeader />
+        <EventDetailedInfo />
+        <EventDetailedChat />
+      </Grid.Column>
+      
+      <Grid.Column width={6}>
+        <EventDetailedSidebar />
+      </Grid.Column>
+    </Grid>
   )
 }
 
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import  {Segment, List, Button, Icon, Item } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 import EventListAttendee from './EventListAttendee'
 
 class EventListItem extends Component {
@@ -43,7 +44,7 @@ class EventListItem extends Component {
                 <Segment clearing>
                 <span>{event.description}</span>
                   <Button onClick={deleteEvents(event.id)} as="a" color="red" floated="right" content="Delete" />
-                  <Button onClick={onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
+                  <Button as={Link} to={`/event/${event.id}`} color="teal" floated="right" content="View" />
                 </Segment>
               </Segment.Group>
       </div>
