3d84413dfe22f714ea05faef490593d4b30ccc70
Oliwier Salamon <salamonoliwier@gmail.com>
2019-01-09 17:45:30 +0100

    added google maps integration

--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Segment, Grid, Icon, Button } from 'semantic-ui-react'
+import EventDetailedMap from './EventDetailedMap'
+class EventDetailedInfo extends Component  {
 
-const EventDetailedInfo = ({event}) => {
+  state = {
+    showMap: false,
+  }
+
+  showMapToggle = () => {
+    this.setState(prevState => ({
+      showMap: !prevState.showMap,
+    }))
+  }
+
+  render(){
+    const {event}=this.props;
     return (
          <Segment.Group>
             <Segment attached="top">
@@ -33,12 +46,16 @@ const EventDetailedInfo = ({event}) => {
                   <span>{event.venue}</span>
                 </Grid.Column>
                 <Grid.Column width={4}>
-                  <Button color="teal" size="tiny" content="Show Map" />
+                  <Button onClick={this.showMapToggle} color="teal" size="tiny" content={this.state.showMap ? 'Hide Map' : 'Show Map'} />
                 </Grid.Column>
               </Grid>
             </Segment>
+
+            {this.state.showMap && 
+            <EventDetailedMap lat={event.venueLatLng.lat} lng={event.venueLatLng.lng} />}
           </Segment.Group>
     )
+  }
 }
 
 export default EventDetailedInfo
